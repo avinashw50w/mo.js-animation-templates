03f64620dc9312a8e2688a3c7535ac5c5b827de8
Avinash Kumar Saw <avinashw50w@gmail.com>
2016-12-21 08:02:46 +0530

    second commit

--- animation 2/animation2.js
+++ animation 2/animation2.js
@@ -1,38 +1,37 @@
-const circle = new mojs.Shape({
-    radius: {0: 200},
-    fill: 'none',
-    stroke: 'yellow',
-    opacity: {1: 0},
-    duration: 1500
-});
+const circ = document.addEventListener('click', function(e) {
+    const circle = new mojs.Shape({
+        radius: {0: 200},
+        fill: 'none',
+        stroke: 'yellow',
+        opacity: {1: 0},
+        duration: 1500
+    }).play();
 
-const circle2 = new mojs.Shape({
-    radius: {0: 200},
-    fill: 'none',
-    stroke: 'yellow',
-    opacity: {1: 0},
-    duration: 1500,
-    delay: 500
-});
+    const circle2 = new mojs.Shape({
+        radius: {0: 200},
+        fill: 'none',
+        stroke: 'yellow',
+        opacity: {1: 0},
+        duration: 1500,
+        delay: 500
+    }).play();
 
-const circle3 = new mojs.Shape({
-    radius: {0: 200},
-    fill: 'none',
-    stroke: 'yellow',
-    opacity: {1: 0},
-    duration: 1500,
-    delay: 1000
-});
+    const circle3 = new mojs.Shape({
+        radius: {0: 200},
+        fill: 'none',
+        stroke: 'yellow',
+        opacity: {1: 0},
+        duration: 1500,
+        delay: 1000
+    }).play();
 
-const circle4 = new mojs.Shape({
-    radius: {0: 200},
-    fill: 'none',
-    stroke: 'yellow',
-    opacity: {1: 0},
-    duration: 1500,
-    delay: 1500
-});
+    const circle4 = new mojs.Shape({
+        radius: {0: 200},
+        fill: 'none',
+        stroke: 'yellow',
+        opacity: {1: 0},
+        duration: 1500,
+        delay: 1500
+    }).play();
 
-const timeline = new mojs.Timeline({
-    repeat: 1500
-}).add(circle, circle2, circle3, circle4).play();
+});
